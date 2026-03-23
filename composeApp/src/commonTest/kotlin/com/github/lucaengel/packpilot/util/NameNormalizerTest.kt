2483af262fed6c96799c043b391bdc52b4e005da
LucaS <luca.engel@epfl.ch>
2026-03-23 13:46:52 +0100

    Refactor `NameNormalizerTest` to use camelCase naming convention

--- composeApp/src/commonTest/kotlin/com/github/lucaengel/packpilot/util/NameNormalizerTest.kt
+++ composeApp/src/commonTest/kotlin/com/github/lucaengel/packpilot/util/NameNormalizerTest.kt
@@ -7,56 +7,56 @@ class NameNormalizerTest {
     private val normalizer: NameNormalizer = DefaultNameNormalizer()
 
     @Test
-    fun normalize_trimsLeadingAndTrailingWhitespaces() {
+    fun normalizeTrimsLeadingAndTrailingWhitespaces() {
         val input = "  Socks  "
         val expected = "socks"
         assertEquals(expected, normalizer.normalize(input))
     }
 
     @Test
-    fun normalize_convertsToLowercase() {
+    fun normalizeConvertsToLowercase() {
         val input = "PHONE Charger"
         val expected = "phone charger"
         assertEquals(expected, normalizer.normalize(input))
     }
 
     @Test
-    fun normalize_reducesMultipleInternalSpacesToSingleSpace() {
+    fun normalizeReducesMultipleInternalSpacesToSingleSpace() {
         val input = "travel    adapter"
         val expected = "travel adapter"
         assertEquals(expected, normalizer.normalize(input))
     }
 
     @Test
-    fun normalize_handlesCombinationOfTrimmingCasingAndSpaceNormalization() {
+    fun normalizeHandlesCombinationOfTrimmingCasingAndSpaceNormalization() {
         val input = "   Extra    SPACES   "
         val expected = "extra spaces"
         assertEquals(expected, normalizer.normalize(input))
     }
 
     @Test
-    fun normalize_returnsEmptyStringForWhitespaceOnlyInput() {
+    fun normalizeReturnsEmptyStringForWhitespaceOnlyInput() {
         val input = "     "
         val expected = ""
         assertEquals(expected, normalizer.normalize(input))
     }
 
     @Test
-    fun normalize_returnsEmptyStringForEmptyInput() {
+    fun normalizeReturnsEmptyStringForEmptyInput() {
         val input = ""
         val expected = ""
         assertEquals(expected, normalizer.normalize(input))
     }
 
     @Test
-    fun normalize_doesNotChangeAlreadyNormalizedStrings() {
+    fun normalizeDoesNotChangeAlreadyNormalizedStrings() {
         val input = "toothbrush"
         val expected = "toothbrush"
         assertEquals(expected, normalizer.normalize(input))
     }
 
     @Test
-    fun normalize_handlesNamesWithMultipleWordsCorrectly() {
+    fun normalizeHandlesNamesWithMultipleWordsCorrectly() {
         val input = "  Power   Bank  "
         val expected = "power bank"
         assertEquals(expected, normalizer.normalize(input))
